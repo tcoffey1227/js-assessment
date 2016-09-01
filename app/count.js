fcf0e8ca1b497b6779947c68e22cb132a823fe18
tcoffey1227 <thomasccoffey@hotmail.com>
2016-09-01 12:35:46 -0500

    updated count
    Updated count file as now I understand what is because asked for.

--- app/count.js
+++ app/count.js
@@ -1,18 +1,29 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function () {
+/*
+	I do question why you used an object called console when that is 
+	kind of a reserved work when in web browsers
+*/
   return {
     count : function (start, end) 
 	{	
-		function sleepFor(sleepDuration)
-		{
-			var now = new Date().getTime();
-			while (new Date().getTime() < now + sleepDuration) { /* do nothing */ }
-		}
-		for (i = start; i < end; i++)
-		{
-			sleepFor(100);
-		}
+		var timeout; 
+		function fred () 
+		{ 
+			console.log(start++); 
+			if (start <= end) 
+			{ 
+				timeout = setTimeout(fred, 100); 
+			} 
+		} 
+		fred(); 
+		return { 
+			cancel: function () 
+			{ 
+				timeout && clearTimeout(timeout); 
+			} 
+		}; 
     }
   };
 });
